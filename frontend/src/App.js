e511c68e75b4b14c0928141bd31843c39daeef5e
mgsaglam <muhammetfeyzijobs@gmail.com>
2020-11-11 21:51:26 +0300

    login tamam

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
-    <>
-      <h1> Welcome To ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+        </Container>
+      </main>
+
+      <Footer />
+    </Router>
   );
 };
 
